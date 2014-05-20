9b61cb5608dca268064028cdc90e934df1226c46
rafael <rafaelvb25@gmail.com>
2014-05-20 01:30:09 -0500

    Tarea 1: completa

---
+++ src/viveros/rafael/unidad1/Alumno.java
@@ -0,0 +1,13 @@
+package viveros.rafael.unidad1;
+
+/**
+ * Created by rafael on 20/05/14.
+ */
+public class Alumno {
+    String nombreMateria;
+    float examenParcial;
+    float practicas;
+    float examenFinal;
+    float asistencia;
+    String nombreAlumno;
+}
