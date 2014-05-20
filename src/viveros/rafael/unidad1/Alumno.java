76266c0a36496cc102c1a9c63ed837b03b9fe5ed
rafael <rafaelvb25@gmail.com>
2014-05-20 01:51:11 -0500

    Tarea 1: tabs en lugar de espacios

--- src/viveros/rafael/unidad1/Alumno.java
+++ src/viveros/rafael/unidad1/Alumno.java
@@ -4,10 +4,10 @@ package viveros.rafael.unidad1;
  * Created by rafael on 20/05/14.
  */
 public class Alumno {
-    String nombreMateria;
-    float examenParcial;
-    float practicas;
-    float examenFinal;
-    float asistencia;
-    String nombreAlumno;
+	String nombreMateria;
+	float examenParcial;
+	float practicas;
+	float examenFinal;
+	float asistencia;
+	String nombreAlumno;
 }
